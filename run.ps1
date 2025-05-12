0fa014f85d59a400e23658c5b0cd45153bb86d98
Cameron Groves <cgrovesp@gmail.com>
2025-05-12 09:47:07 -0500

    fixed the link for the bat scrypt

--- run.ps1
+++ run.ps1
@@ -7,7 +7,7 @@ mkdir C:\TEMP
 $ProgressPreference = "SilentlyContinue"
 wget "https://is0s.file.core.windows.net/labtech/S1_24.2.exe?sv=2023-11-03&st=2025-05-09T22%3A20%3A32Z&se=2026-05-30T22%3A20%3A00Z&sr=f&sp=r&sig=V4M7qOd4Lg6ZcOBhYCSv9xJ2W3EAO752TwL1JVEtd3A%3D" -OutFile C:\TEMP\s1.exe
 
-wget "https://is0s.file.core.windows.net/labtech/safe.bat?sv=2023-11-03&st=2025-05-09T22%3A40%3A27Z&se=2025-11-15T23%3A40%3A00Z&sr=f&sp=r&sig=J%2BrnJA2QKoATJ%2FDPK1CTGR7fpWEU4IMuAv5AjOm4P4o%3D" -OutFile C:\TEMP\safe.bat
+wget "https://raw.githubusercontent.com/Omahait/FullAutoS1Clean/refs/heads/main/safe.bat" -OutFile C:\TEMP\safe.bat
 
 cmd.exe /c "bcdedit /set {default} safeboot network"
 

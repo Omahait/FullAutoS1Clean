517fc8845ed40803a70cd7e7065b85caf619d5ce
Scynthane <159321584+Scynthane@users.noreply.github.com>
2025-05-16 15:39:27 -0500

    Added admin auto-logon removal

--- safe.bat
+++ safe.bat
@@ -3,6 +3,11 @@ cls
  
 :: Disable Safe Mode
 bcdedit /deletevalue {default} safeboot
+
+:: Disable auto-logon 
+REG DELETE "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /f
+REG DELETE "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUsername /f
+REG DELETE "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /f
  
 :: Reboot the system
 shutdown /r /f /t 600

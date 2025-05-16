27e95d38342fc4633217e56f43685ebf22f8273b
Scynthane <159321584+Scynthane@users.noreply.github.com>
2025-05-16 16:43:34 -0500

    remove AutoLogon64.exe

--- safe.bat
+++ safe.bat
@@ -5,9 +5,7 @@ cls
 bcdedit /deletevalue {default} safeboot
 
 :: Disable auto-logon 
-REG DELETE "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /f
-REG DELETE "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUsername /f
-REG DELETE "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /f
+del C:\TEMP\AutoLogon64.exe /f
  
 :: Reboot the system
 shutdown /r /f /t 600

66eb9c50f02dd5e6c50c176d1d3b076a95ab4a3c
Scynthane <159321584+Scynthane@users.noreply.github.com>
2025-05-19 16:44:47 -0500

    Update safe.bat
    updated reboot time and removed references to autologon

--- safe.bat
+++ safe.bat
@@ -3,9 +3,6 @@ cls
  
 :: Disable Safe Mode
 bcdedit /deletevalue {default} safeboot
-
-:: Disable auto-logon 
-del C:\TEMP\AutoLogon64.exe /f
  
 :: Reboot the system
-shutdown /r /f /t 600
+shutdown /r /f /t 60
